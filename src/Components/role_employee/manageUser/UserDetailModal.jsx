2e02100ec05871542f77b300b9c7ac178bad408f
DanaiLT <danaiseangbourmad@gmail.com>
2024-05-26 13:24:17 +0700

    design Add money to Atm and Delete Account

--- src/Components/role_employee/manageUser/UserDetailModal.jsx
+++ src/Components/role_employee/manageUser/UserDetailModal.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { RxCross2 } from "react-icons/rx";
+import { MdDeleteOutline } from "react-icons/md";
 
 const UserDetailModal = ({ closeModal, selectedUser }) => {
 
@@ -7,9 +9,48 @@ const UserDetailModal = ({ closeModal, selectedUser }) => {
     return (
         <div>
             <div className='fixed flex start-0 top-0 justify-center z-20 bg-black bg-opacity-30 h-screen w-full '>
-                <div className='relative bg-white rounded-lg max-w-4xl min-h-[70vh] w-full my-auto p-8'>
-                    <p className='text-black cursor-pointer' onClick={closeModal}>ปิด</p>
-                    <p className='text-black'>{selectedUser.FirstName}</p>
+                <div className='relative bg-white rounded-lg max-w-4xl min-h-[60vh] w-full my-auto'>
+                    <div className='flex justify-between items-center border-b border-slate-400 w-full px-6 py-2 '>
+                        <p className='text-xl'>รายละเอียดข้อมูล</p>
+                        <div className='text-black cursor-pointer' onClick={closeModal}>
+                            <RxCross2 className='text-xl' />
+                        </div>
+                    </div>
+                    <div className='py-8 px-24 grid grid-cols-3'>
+                        <div className='col-span-1 font-bold text-xl '>
+                            <p className='my-3'>Account Id</p>
+                            <p className='my-3'>User Id</p>
+                            <p className='my-3'>เลขบัญชี</p>
+                            <p className='my-3'>ชื่อ-นามสกุล</p>
+                            <p className='my-3'>วันที่เปิดบัญชี</p>
+                            <p className='my-3'>E-mail</p>
+                            <p className='my-3'>เลขบัตรประชาชน</p>
+                            <p className='my-3'>เบอร์โทรศัพท์</p>
+                        </div>
+                        <div className='col-span-2 font-medium text-xl text-slate-500'>
+                            <p className='my-3'>{selectedUser.AccountID}</p>
+                            <p className='my-3'>{selectedUser.UserID}</p>
+                            <p className='my-3'>{selectedUser.AccountNumber}</p>
+                            <p className='my-3'>{selectedUser.FirstName} {selectedUser.LastName}</p>
+                            <p className='my-3'>{selectedUser.DateOpen}น.</p>
+                            <p className='my-3'>{selectedUser.Email}</p>
+                            <p className='my-3'>{selectedUser.IdCard}</p>
+                            <p className='my-3'>{selectedUser.PhoneNumber}</p>
+                        </div>
+                        <div className='col-span-1 font-bold text-xl'>
+                            <p className='my-3'>จำนวนเงินในบัญชี</p>
+                        </div>
+                        <div className='col-span-2 font-medium text-xl text-slate-500'>
+                            <p className='my-3'>{selectedUser.Balance} บาท</p>
+                        </div>
+                    </div>
+                    <p className='text-red-600 ms-24 -mt-6'>คำเตือน: กรุณาตรวจสอบให้เเน่ใจว่าไม่มีเงินอยู่ในบัญชีเเล้วเมื่อต้องการที่จะลบบัญชี</p>
+                    <div className='flex justify-center w-auto mt-4'>
+                        <div className='flex items-center text-white font-medium rounded-md px-5 py-2 bg-red-500'>
+                            <MdDeleteOutline />
+                            <p>ลบบัญชี</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
